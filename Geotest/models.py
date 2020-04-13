fb24ca5f3a0e6de64e6d9fd7ef24f8605a3d0d4c
claudiopietra <63510146+claudiopietra@users.noreply.github.com>
2020-04-13 19:07:51 +0200

    Location Inventory

--- Geotest/models.py
+++ Geotest/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Location(models.Model):
     locationname = models.CharField(max_length=255)
     locationidx = models.CharField(max_length=255)
+    in_inventory = models.CharField(max_length=255)
+    not_in_inventory = models.CharField(max_length=255)
     htmlbody = models.TextField()
 
     def __str__(self):
