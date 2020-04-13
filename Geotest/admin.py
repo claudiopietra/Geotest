000de96aa9b3667a634b28faed1ca3bc483dc48c
claudiopietra <63510146+claudiopietra@users.noreply.github.com>
2020-04-13 16:32:00 +0200

    Update admin.py

--- Geotest/admin.py
+++ Geotest/admin.py
@@ -3,7 +3,8 @@ from .models import Location
 from .models import Item
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display('itemidx', 'itemname')
+    #list_display('itemidx', 'itemname')
+    pass
 
 admin.site.register(Location)
 admin.site.register(Item, ItemAdmin)
