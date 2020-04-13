26ef4567e3f3e7cc19ecd65a6420721ecab859cd
claudiopietra <63510146+claudiopietra@users.noreply.github.com>
2020-04-13 17:48:50 +0200

    Session Managment

--- Geotest/businesslogic.py
+++ Geotest/businesslogic.py
@@ -4,8 +4,10 @@
 def get_session(request):
     session = request.session
     if not session.get('initialized', False):
+        #---Session should expire in 24h = 86400s
+        session.set_expiry(86400)
         session['initialized'] = True
         session['inventory'] = []
         session['wrongurls'] = 0
-        
+
     return session
