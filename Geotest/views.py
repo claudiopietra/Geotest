930fdf57edda7e642f336a28d8ebfc49aeab1fe8
claudiopietra <63510146+claudiopietra@users.noreply.github.com>
2020-04-13 17:08:02 +0200

    Update views.py

--- Geotest/views.py
+++ Geotest/views.py
@@ -24,7 +24,7 @@ def display_location(request, location_id):
 
 def not_valid(request, entered_url):
     session = get_session(request)
-    return return HttpResponse('hier ist nichts. Session=%s' %(str(session)))
+    return HttpResponse('hier ist nichts. Session=%s' %str(session))
     
     #session['wrongurls'] += 1
     #return HttpResponse('hier ist nichts. Anzahl Fehlversuche: %s. Session=%s' %(str(session['wrongurls']), str(session)))
